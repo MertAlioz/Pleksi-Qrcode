5147429158e77f1a7c6ab7a81d780e06bd4b9e4c
Mert <ozmert2434@gmail.com>
2021-07-26 21:18:35 +0300

    fix

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -15,8 +15,6 @@ const initialState = {
     logoMw: 320,
     logoMh: 60,
     Qrcodes: [],
-    Anana = "",
-    
 }
 const reducer = (state = initialState, action) => {
     switch (action.type) {
