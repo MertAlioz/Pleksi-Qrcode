a8142cbc54f2a68cfaf153caa81b97732b2b1254
Mert Ali ÖZ <63559486+MertAlioz@users.noreply.github.com>
2021-11-16 17:00:46 +0300

    ..
    Deletet Old Folders

--- src/store/method/index.js
+++
@@ -1,8 +0,0 @@
-export const UPDATE_CANVAS = "UpdateCanvas";
-export const QR_EDIT = "QrEdit";
-export const BRANCHEDIT = "branchEdit";
-export const QRADD = "Qradd";
-export const START = "start";
-export const LOGOEDIT = "LOGOEDIT";
-export const NEWLOGO = "NEWLOGO";
-
